c42592c9b5bdcc176423e38697573c8e72fcc403
BamH1 <jo.schultheis@web.de>
2023-07-14 15:51:58 +0200

    small updates, mapping of weekdays to standard of Unfallatlas

--- date_states.py
+++ date_states.py
@@ -32,30 +32,41 @@ day = today.day
 weekday = today.weekday()
 hour = today.hour
 print(year)
-print(weekday)
+print(f' weekday: {weekday}')
 print(month)
 print(hour)
 
+# map weekdays to standard of Unfallatlas since they start with 1 on sunday opposite to open-meteo 0 on monday
+weekday_dict = {0:2, 1:3, 2:4, 3:5, 4:6, 5:7, 6:1}
+weekday = weekday_dict[weekday]
+print(f' weekday: {weekday}')
+
 today_weather_format = f"{year}-{month}-{day}T{hour}:00"
 print(today_weather_format)
-
-weather = weather_data["hourly"]["weathercode"][hour]
-weather_minus1 = weather_data["hourly"]["weathercode"][hour-1]
+weather = set([])
+print(weather)
+weather.add(weather_data["hourly"]["weathercode"][hour])
+weather.add(weather_data["hourly"]["weathercode"][hour-1])
 # or turn weather into list?
 # TODO: check for data at midnight (00-1)
 print(weather)
 
-ww_freezing = [56, 57, 66, 67, 71, 73, 75, 77, 85, 86]
-ww_wet = [51, 53, 55, 61, 63, 65, 80, 81, 82, 95, 96, 99]
-flag_freezing = False
-flag_wet = False
-flag_weather_cond = 0
-if weather in ww_freezing:
-    flag_freezing = True
-    flag_weather_cond = 0
+ww_freezing = {56, 57, 66, 67, 71, 73, 75, 77, 85, 86}
+ww_wet = {51, 53, 55, 61, 63, 65, 80, 81, 82, 95, 96, 99}
+
+weather_cond = 0
+if weather.intersection(ww_wet):
+    weather_cond = 1
+if weather.intersection(ww_freezing):
+    weather_cond = 2
+
 if weather in ww_wet:
     flag_wet = True
-    flag_weather_cond = 0
+    weather_cond = 1
+if weather in ww_freezing:
+    flag_freezing = True
+    flag_weather_cond = 2
+
 
 
 ### extend request for additional data
@@ -72,4 +83,5 @@ if weather in ww_wet:
 # TODO: convert weather data to format used in dataframe
 
 
-#
+# get df
+# df = pd.read_csv('Unfallorte_Muenster_comb.csv', encoding='ISO-8859-1')
